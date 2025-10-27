885f62f2b027c089e96e05f59818b9f87402ce98
FLighter7 <21360067+FLighter7@users.noreply.github.com>
2025-10-27 17:26:23 +0300

    feat(page-shared): update page

--- src/pages/shared.vue
+++ src/pages/shared.vue
@@ -1,3 +1,18 @@
 <template>
-	<VContainer>Shared</VContainer>
+	<VSheet>
+		<h2>Shared / All</h2>
+		<p class="mb-4 text-grey-darken-1">
+			Contains common backend and frontend parts for all applications.
+		</p>
+
+		<h2>Shared / Backend</h2>
+		<p class="mb-4 text-grey-darken-1">
+			Contains common backend parts for all applications.
+		</p>
+
+		<h2>Shared / Frontend</h2>
+		<p class="mb-4 text-grey-darken-1">
+			Contains common frontend parts for all applications.
+		</p>
+	</VSheet>
 </template>
